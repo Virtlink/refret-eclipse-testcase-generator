722d7ddbfbcae71744a0b77b18f191e884c0c852
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2023-02-24 11:35:40 +0100

    Change project type

--- generator/build.gradle.kts
+++ generator/build.gradle.kts
@@ -1,7 +1,8 @@
 // Workaround for IntelliJ issue where `libs` is errored: https://youtrack.jetbrains.com/issue/KTIJ-19369
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    `java-library`  // Or: java
+    java
+    `maven-publish`
     alias(libs.plugins.kotlin.jvm)
 }
 
