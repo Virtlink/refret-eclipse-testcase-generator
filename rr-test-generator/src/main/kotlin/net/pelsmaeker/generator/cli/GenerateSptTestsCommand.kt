41ad775093f025de83639df113feb03773305664
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2023-02-25 11:41:23 +0100

    Add dummy command

---
+++ rr-test-generator/src/main/kotlin/net/pelsmaeker/generator/cli/GenerateSptTestsCommand.kt
@@ -0,0 +1,14 @@
+package net.pelsmaeker.generator.cli
+
+import com.github.ajalt.clikt.core.CliktCommand
+
+/** Generate SPT test files command. */
+class GenerateSptTestsCommand: CliktCommand(
+    name = "generate-spt-tests",
+    help = "Generate SPT test files"
+) {
+    override fun run() {
+        TODO()
+        println("Done!")
+    }
+}
